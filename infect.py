b11347084cbdcc4d413dbe22c07a5fc4be24a45f
EscobarWilliam <williamj915@gmail.com>
2024-05-27 16:01:55 -0700

    Pushing comments to infect.py

--- infect.py
+++ infect.py
@@ -7,8 +7,14 @@ def infect_step(G,p1,individuals,N):
     G (ndarray N*N): the adjacency matrix.
     p1: the probability each individual infects neighbours.
     '''
-
-    individuals_updated = np.copy(individuals)
+    
+    # Process: 
+    # Here we use the given adjacency matrix to incorporate the two setep 
+    # infection. We will step through all elements of the matrix and see 
+    # which individuals are connected. If connected to anyone (i.e. G_ij = 1)
+    # then with probability p1 we can infect them, otherwise move on. 
+    
+    individuals_updated = np.copy(individuals) #make a copy of the individuals array
     for i in range(N):
         if individuals[i] == 1:  # Check if individual i is infected
             for j in range(N):
@@ -28,14 +34,15 @@ def infect(G,p0,p1,time_steps):
     p1: the probability each individual infects neighbours.
     time_steps: log N
     '''
-    N = G.shape[0]
-    individuals = np.zeros(N)
+    N = G.shape[0] #get the number of rows (i.e. N from G)
+    individuals = np.zeros(N) #initially everyone is not infected
     ###################################################
     # Initially infect some individuals randomly based on p0
     for i in range(N):
         if random.random() < p0:
             individuals[i] = 1
     
+    # Now call the 2-step infection to also infect neighbors with prob  p1
     for _ in range(time_steps):
         individuals = infect_step(G, p1, individuals, N)
     
